515c845f933f54287a63bcfd30728604c26ca26b
Chris Luedtke <chrisluedtke@gmail.com>
2019-02-11 20:23:12 -0600

    exception handling when missing credentials.ini

--- cyautomation/cyschoolhouse/config.py
+++ cyautomation/cyschoolhouse/config.py
@@ -10,6 +10,7 @@ __all__ = [
     'SF_USER',
     'SF_PASS',
     'SF_TOKN',
+    'INPUT_PATH',
     'LOG_PATH',
     'TEMP_PATH',
     'TEMPLATES_PATH',
@@ -22,7 +23,15 @@ SANDBOX = False
 
 creds_path = str(Path(__file__).parent / 'credentials.ini')
 config = ConfigParser()
-config.read(creds_path)
+
+try:
+    open(creds_path)
+except FileNotFoundError:
+    raise FileNotFoundError('Before you can use this pacakge, you must create '
+                            'a credentials.ini file. See the README for '
+                            'details.')
+else:
+    config.readfp(open(creds_path))
 
 if SANDBOX == False:
     SF_URL = "https://na82.salesforce.com"
@@ -35,11 +44,14 @@ SF_USER = sf_creds['username']
 SF_PASS = sf_creds['password']
 SF_TOKN = sf_creds['security_token']
 
+INPUT_PATH = str(Path(__file__).parent / 'input')
 LOG_PATH = str(Path(__file__).parent / 'log')
 TEMP_PATH = str(Path(__file__).parent / 'temp')
 TEMPLATES_PATH = str(Path(__file__).parent / 'templates')
+SCH_REF_PATH = ('Z:/ChiPrivate/Chicago Data and Evaluation/'
+                'SY19/SY19 School Reference.xlsx')
 
-def get_sch_ref_df(sch_df_path = 'Z:\\ChiPrivate\\Chicago Data and Evaluation\\SY19\\SY19 School Reference.xlsx'):
+def get_sch_ref_df(sch_df_path=SCH_REF_PATH):
     sch_ref_df = pd.read_excel(sch_df_path)
     sch_ref_df = sch_ref_df.loc[~sch_ref_df['Informal Name'].isin(['CE', 'Onboarding'])]
 
