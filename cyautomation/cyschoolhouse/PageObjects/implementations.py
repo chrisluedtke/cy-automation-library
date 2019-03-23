a98d49a3631d91f7b67c28d987a398fd217254f9
Chris Luedtke <chrisluedtke@gmail.com>
2019-03-22 19:06:19 -0500

    fixed class inheritance and formatting

--- cyautomation/cyschoolhouse/PageObjects/implementations.py
+++ cyautomation/cyschoolhouse/PageObjects/implementations.py
@@ -16,24 +16,6 @@ from ..config import (
 
 GECKO_PATH = str(Path(__file__).parents[3] / 'geckodriver/geckodriver.exe')
 
-# def extract_key():
-#     """Extract SSO Information from keyfile
-#
-#     One practice I use here is to store my SSO in a keyfile instead of either
-#     entering it in the cmd prompt on each run or hardcoding it in the program.
-#     Keyfile should be formatted like so:
-#         'descript:cityyear sso/user:aperusse/pass:p@ssw0rd'
-#     Simply replace the username and password with your credentials and then save
-#     it as keyfile.txt
-#     """
-#     key_file_path = str(Path(__file__).parent.parent / 'credentials.ini')
-#     with open(key_file_path) as file:
-#         keys = file.read()
-#     split_line = keys.split("/")
-#     entries = [item.split(":")[1] for item in split_line]
-#     desc, user, pwd = entries
-#     return user, pwd
-
 
 class BaseImplementation(object):
     """Base Implementation object
@@ -63,24 +45,25 @@ class BaseImplementation(object):
 
 
 class Okta(BaseImplementation):
-    """Object for handingling Okta
+    """Object for handling Okta
 
     Wraps all processes for logging into Okta and navigation.
     """
-    # user, pwd = extract_key()
-    user = OKTA_USER
-    pwd = OKTA_PASS
+    def __init__(self):
+        super().__init__()
+        self.user = OKTA_USER
+        self.pwd = OKTA_PASS
 
     def set_up(self):
         """Navigate to City Year Okta login"""
         self.driver.get("https://cityyear.okta.com")
 
-    def enter_credentials(self, username, password):
+    def enter_credentials(self):
         """Enter credentials for Okta Login"""
         login_page = page.OktaLoginPage(self.driver)
         assert login_page.page_is_loaded()
-        login_page.username = username
-        login_page.password = password
+        login_page.username = self.user
+        login_page.password = self.pwd
         login_page.click_login_button()
 
     def check_logged_in(self):
@@ -91,7 +74,7 @@ class Okta(BaseImplementation):
     def login(self):
         """Runs all steps to login to Okta"""
         self.set_up()
-        self.enter_credentials(self.user, self.pwd)
+        self.enter_credentials()
         self.check_logged_in()
 
     def launch_cyschoolhouse(self):
@@ -103,26 +86,13 @@ class Okta(BaseImplementation):
         assert Okta.page_is_loaded()
         Okta.launch_cyschoolhouse()
 
-# class SectionEnrollment(Okta):
-#     """Implementation script for Section Enrollment"""
-#     from pandas import read_excel
-#
-#     data = read_excel('input_files/student-enrollment-records.xlsx')
-#
-#     def search_for_a_section(self, section):
-#         """Should only be used from the cyschoohouse homepage"""
-#         cysh_home = page.CyshHomePage(self.driver)
-#         assert cysh_home.page_is_loaded()
-#         cysh_home.set_search_filter("Sections")
-#         cysh_home.search_bar = section
-#         cysh_home.click_search_button()
 
 class IndicatorAreaEnrollment(Okta):
     """Implementation object for Indicator Area Enrollment"""
 
     def __init__(self):
         from pandas import read_excel
-
+        super().__init__()
         xl_path = str(Path(INPUT_PATH) / 'indicator_area_roster.xlsx')
         self.data = read_excel(xl_path)
         self.student_list = self.data['Student: Student ID'].unique()
@@ -187,14 +157,17 @@ class IndicatorAreaEnrollment(Okta):
 
             try:
                 self.enroll_student(student_id)
+            except KeyboardInterrupt:
+                return None
+
             except TimeoutException:
-                print("Timeout Failure on student: {}".format(student_id))
+                print(f"Timeout Failure on student: {student_id}")
                 self.error_count += 1
 
             except StaleElementReferenceException:
-                print("Stale Element failure on student: {}".format(student_id))
+                print(f"Stale Element failure on student: {student_id}")
                 self.error_count += 1
 
-            except:
-                print("Caught a {} error on student: {}".format(sys.exc_info(), student_id))
+            except Exception as e:
+                print(f"Caught a {e} error on student: {student_id}")
                 self.error_count += 1
