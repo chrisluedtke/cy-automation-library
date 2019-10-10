2849dc407cee0395b57694b26cdf5c62f3ab8820
Chris Luedtke <chrisluedtke@gmail.com>
2019-10-10 16:27:34 -0500

    formatting

--- cyautomation/cyschoolhouse/tot_audit.py
+++ cyautomation/cyschoolhouse/tot_audit.py
@@ -1,10 +1,9 @@
-import os
+from pathlib import Path
 
 import pandas as pd
-# from StyleFrame import StyleFrame, Styler, utils
 
-from .config import get_sch_ref_df
 from . import simple_cysh as cysh
+from .config import YEAR, get_sch_ref_df
 
 
 def fix_T1T2ELT(sf=cysh.sf):
@@ -12,57 +11,83 @@ def fix_T1T2ELT(sf=cysh.sf):
     """
 
     typo_map = {
-        r'([Tt]([Ii][Ee]|[Ee][Ii])[Rr] ?|t)(1|[Oo]ne)':'T1',
-        r'([Tt]([Ii][Ee]|[Ee][Ii])[Rr] ?|t)(2|[Tt]wo)':'T2',
-        #r'([Aa]fter ?[Ss]chool|ASP)':'ELT',
+        r'([Tt]([Ii][Ee]|[Ee][Ii])[Rr] ?|t)(1|[Oo]ne)': 'T1',
+        r'([Tt]([Ii][Ee]|[Ee][Ii])[Rr] ?|t)(2|[Tt]wo)': 'T2',
+        #r'([Aa]fter ?[Ss]chool|ASP)': 'ELT',
     }
 
     all_typos = '|'.join(list(typo_map.keys()))
 
     df = cysh.get_object_df('Intervention_Session__c', ['Id', 'Comments__c'],
                             rename_id=True)
-    df['Comments__c'].fillna('', inplace=True)
+    df['Comments__c'] = df['Comments__c'].fillna('', inplace=True)
     df = df.loc[df['Comments__c'].str.contains(all_typos)]
 
-    df['Comments__c'].replace(typo_map, regex=True, inplace=True)
+    df['Comments__c'] = df['Comments__c'].replace(typo_map, regex=True)
 
     print(f"Found {len(df)} T1, T2, or ELT labels that can be fixed")
 
     results = []
     for _, row in df.iterrows():
-        result = sf.Intervention_Session__c.update(row.Intervention_Session__c,
-                                                   {'Comments__c':row['Comments__c']})
+        result = sf.Intervention_Session__c.update(
+            row.Intervention_Session__c,
+            {'Comments__c':row['Comments__c']}
+        )
         results.append(result)
 
     return results
 
+
 def get_error_table():
-    ISR_df = cysh.get_object_df('Intervention_Session_Result__c',
-                                ['Amount_of_Time__c', 'IsDeleted',
-                                'Intervention_Session_Date__c',
-                                'Related_Student_s_Name__c',
-                                'Intervention_Session__c', 'CreatedDate'])
-    IS_df = cysh.get_object_df('Intervention_Session__c',
-                               ['Id', 'Name', 'Comments__c', 'Section__c'],
-                               rename_id=True, rename_name=True)
-    section_df = cysh.get_object_df('Section__c', ['Id', 'School__c', 'Intervention_Primary_Staff__c', 'Program__c'], rename_id=True)
-
+    ISR_df = cysh.get_object_df(
+        'Intervention_Session_Result__c',
+        ['Amount_of_Time__c', 'IsDeleted', 'Intervention_Session_Date__c',
+        'Related_Student_s_Name__c', 'Intervention_Session__c', 'CreatedDate']
+    )
+    IS_df = cysh.get_object_df(
+        'Intervention_Session__c',
+        ['Id', 'Name', 'Comments__c', 'Section__c'],
+        rename_id=True, rename_name=True
+    )
+    section_df = cysh.get_object_df(
+        'Section__c', 
+        ['Id', 'School__c', 'Intervention_Primary_Staff__c', 'Program__c'], 
+        rename_id=True
+    )
     school_df = cysh.get_object_df('Account', ['Id', 'Name'])
-    school_df.rename(columns={'Id':'School__c', 'Name':'School_Name__c'}, inplace=True)
+    school_df = school_df.rename(columns={
+        'Id': 'School__c', 
+        'Name': 'School_Name__c'}
+    )
+    staff_df = cysh.get_object_df(
+        'Staff__c', 
+        ['Id', 'Name'], 
+        where="Site__c = 'Chicago'", 
+        rename_id=True, rename_name=True
+    )
+    program_df = cysh.get_object_df(
+        'Program__c', 
+        ['Id', 'Name'], 
+        rename_id=True, rename_name=True
+    )
 
-    staff_df = cysh.get_object_df('Staff__c', ['Id', 'Name'], where="Site__c = 'Chicago'", rename_id=True, rename_name=True)
+    df = (ISR_df.merge(IS_df, how='left', on='Intervention_Session__c')
+                .drop(columns=['Intervention_Session__c'])
+                .merge(section_df, how='left', on='Section__c')
+                .drop(columns=['Section__c'])
+                .merge(school_df, how='left', on='School__c')
+                .drop(columns=['School__c'])
+                .merge(staff_df, how='left', 
+                       left_on='Intervention_Primary_Staff__c',
+                       right_on='Staff__c')
+                .drop(columns=['Intervention_Primary_Staff__c', 'Staff__c'])
+                .merge(program_df, how='left', on='Program__c')
+                .drop(columns=['Program__c']))
 
-    program_df = cysh.get_object_df('Program__c', ['Id', 'Name'], rename_id=True, rename_name=True)
-
-    df = ISR_df.merge(IS_df, how='left', on='Intervention_Session__c'); del df['Intervention_Session__c']
-    df = df.merge(section_df, how='left', on='Section__c'); del df['Section__c']
-    df = df.merge(school_df, how='left', on='School__c'); del df['School__c']
-    df = df.merge(staff_df, how='left', left_on='Intervention_Primary_Staff__c', right_on='Staff__c'); del df['Intervention_Primary_Staff__c'], df['Staff__c']
-    df = df.merge(program_df, how='left', on='Program__c'); del df['Program__c']
-
-    df['Intervention_Session_Date__c'] = pd.to_datetime(df['Intervention_Session_Date__c']).dt.date
+    df['Intervention_Session_Date__c'] = \
+        pd.to_datetime(df['Intervention_Session_Date__c']).dt.date
     df['CreatedDate'] = pd.to_datetime(df['CreatedDate']).dt.date
-    df['Comments__c'].fillna('', inplace=True)
+    df['Comments__c'] = df['Comments__c'].fillna('', inplace=True)
 
     df.loc[df['Program__c_Name'].str.contains('Tutoring')
            & ~df['Comments__c'].str.contains('T1|T2'), 'Missing T1/T2 Code'] = 'Missing T1/T2 Code'
@@ -87,8 +112,8 @@ def get_error_table():
     df['Error'] = df[error_cols].apply(lambda x: x.str.cat(sep=' & '), axis=1)
 
     accepted_errors_df = pd.read_excel((
-        "Z:/ChiPrivate/Chicago Data and Evaluation/SY19/"
-        "SY19 ToT Audit Accepted Errors.xlsx"
+        f"Z:/ChiPrivate/Chicago Data and Evaluation/{YEAR}/"
+        f"{YEAR} ToT Audit Accepted Errors.xlsx"
     ))
 
     df = df.loc[
@@ -109,46 +134,22 @@ def get_error_table():
         'Error':'Error',
     }
 
-    df = df.rename(columns=col_friendly_names)
-    df = df.sort_values(list(col_friendly_names.values()))
-    df = df[list(col_friendly_names.values())]
+    df = (df.rename(columns=col_friendly_names)
+            .sort_values(list(col_friendly_names.values())))
 
-    return df
+    return df[list(col_friendly_names.values())]
+
 
 def write_error_tables_to_cyconnect(df):
     sch_ref_df = get_sch_ref_df()
-    for index, row in sch_ref_df.iterrows():
+    for _, row in sch_ref_df.iterrows():
         school_error_df = df.loc[df['School'] == row['School']].copy()
         del school_error_df['School']
 
-        write_path = (f"Z:/{row['Informal Name']} Team Documents/SY19 ToT Audit "
-                      f"Errors - {row['Informal Name']}.xlsx")
+        write_path = (f"Z:/{row['Informal Name']} Team Documents/"
+                      f"{YEAR} ToT Audit Errors - {row['Informal Name']}.xlsx")
 
-        if os.path.exists(write_path):
-            os.remove(write_path)
+        if Path(write_path).exists():
+            Path(write_path).unlink()
 
         school_error_df.to_excel(write_path, index=False)
-
-        # excel_writer = StyleFrame.ExcelWriter(write_path)
-        # sfr = StyleFrame(school_error_df)
-        # sfr.apply_column_style(
-        #     cols_to_style=list(school_error_df),
-        #     styler_obj=Styler(
-        #         horizontal_alignment=utils.horizontal_alignments.left,
-        #         #vertical_alignment=utils.vertical_alignments.top
-        #     ),
-        #     width=25,
-        #     style_header=False,
-        # )
-        #
-        # if len(school_error_df) > 0:
-        #     freeze = 'A2'
-        # else:
-        #     freeze = 'A1'
-        #
-        # sfr.to_excel(
-        #     excel_writer=excel_writer,
-        #     row_to_add_filters=0,
-        #     columns_and_rows_to_freeze=freeze,
-        # )
-        # excel_writer.save()
