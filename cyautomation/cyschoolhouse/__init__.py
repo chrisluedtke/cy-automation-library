22f6bd4a379c9f2925a8fce300bfbbad437dea74
Chris Luedtke <chrisluedtke@gmail.com>
2019-10-15 13:15:38 -0500

    update ToT Audit to new API

--- cyautomation/cyschoolhouse/__init__.py
+++ cyautomation/cyschoolhouse/__init__.py
@@ -2,13 +2,13 @@
 from . import section_creation, student, student_section
 from .config import USER_SITE, get_sch_ref_df
 from .simple_cysh import (get_object_df, get_object_fields, get_section_df,
-                          get_staff_df, get_student_section_staff_df,
-                          get_student_df, init_sf_session, object_reference, 
-                          sf)
+                          get_staff_df, get_student_df,
+                          get_student_section_staff_df, init_sf_session,
+                          object_reference, sf)
 
 if USER_SITE.lower() == 'chicago':
     from . import ia_assignment_chi
     from . import section_creation_chi
-    from . import tot_audit
+    from .tot_audit import ToTAudit
     from .trackers import (AttendanceTracker, CoachingLog, 
                            LeadershipTracker, WeeklyServiceTracker)
--- cyautomation/cyschoolhouse/tot_audit.py
+++ cyautomation/cyschoolhouse/tot_audit.py
@@ -3,153 +3,214 @@ from pathlib import Path
 import pandas as pd
 
 from . import simple_cysh as cysh
-from .config import YEAR, get_sch_ref_df
+from .config import TEMP_PATH, YEAR, get_sch_ref_df, set_logger
 
+logger = set_logger(name=Path(__file__).stem)
 
-def fix_T1T2ELT(sf=cysh.sf):
-    """ Standardize common spellings of "T1" "T2" and "ELT"
-    """
+class ToTAudit:
+    def __init__(self, kind='ToT Audit Errors', folder='Team Documents', 
+                 filetype='.xlsx', test=False):
+        self.kind = kind
+        self.sch_ref_df = get_sch_ref_df()
+        self.test = test
 
-    typo_map = {
-        r'([Tt]([Ii][Ee]|[Ee][Ii])[Rr] ?|t)(1|[Oo]ne)': 'T1',
-        r'([Tt]([Ii][Ee]|[Ee][Ii])[Rr] ?|t)(2|[Tt]wo)': 'T2',
-        #r'([Aa]fter ?[Ss]chool|ASP)': 'ELT',
-    }
+        if self.test:
+            root_dir = TEMP_PATH
+        else:
+            root_dir = 'Z:/'
 
-    all_typos = '|'.join(list(typo_map.keys()))
-
-    df = cysh.get_object_df('Intervention_Session__c', ['Id', 'Comments__c'],
-                            rename_id=True)
-    df['Comments__c'] = df['Comments__c'].fillna('', inplace=True)
-    df = df.loc[df['Comments__c'].str.contains(all_typos)]
-
-    df['Comments__c'] = df['Comments__c'].replace(typo_map, regex=True)
-
-    print(f"Found {len(df)} T1, T2, or ELT labels that can be fixed")
-
-    results = []
-    for _, row in df.iterrows():
-        result = sf.Intervention_Session__c.update(
-            row.Intervention_Session__c,
-            {'Comments__c':row['Comments__c']}
+        self.sch_ref_df['tracker_path'] = self.sch_ref_df.apply(
+            lambda df: (
+                Path(root_dir) / f"{df['Informal Name']} {folder}" / 
+                f"{YEAR} {self.kind} - {df['Informal Name']}{filetype}"
+            ),
+            axis=1
         )
-        results.append(result)
+        self.sch_ref_df = self.sch_ref_df.set_index('School')
 
-    return results
+    def deploy_all(self):
+        """ Fixes typos and distributes audit for all school teams.
 
+        Returns a summary dataframe.
+        """
+        self.fix_T1T2ELT_typos()
+        errors_df = self.get_errors_df()
 
-def get_error_table():
-    ISR_df = cysh.get_object_df(
-        'Intervention_Session_Result__c',
-        ['Amount_of_Time__c', 'IsDeleted', 'Intervention_Session_Date__c',
-        'Related_Student_s_Name__c', 'Intervention_Session__c', 'CreatedDate']
-    )
-    IS_df = cysh.get_object_df(
-        'Intervention_Session__c',
-        ['Id', 'Name', 'Comments__c', 'Section__c'],
-        rename_id=True, rename_name=True
-    )
-    section_df = cysh.get_object_df(
-        'Section__c', 
-        ['Id', 'School__c', 'Intervention_Primary_Staff__c', 'Program__c'], 
-        rename_id=True
-    )
-    school_df = cysh.get_object_df('Account', ['Id', 'Name'])
-    school_df = school_df.rename(columns={
-        'Id': 'School__c', 
-        'Name': 'School_Name__c'}
-    )
-    staff_df = cysh.get_object_df(
-        'Staff__c', 
-        ['Id', 'Name'], 
-        where="Site__c = 'Chicago'", 
-        rename_id=True, rename_name=True
-    )
-    program_df = cysh.get_object_df(
-        'Program__c', 
-        ['Id', 'Name'], 
-        rename_id=True, rename_name=True
-    )
+        for school in self.sch_ref_df.index:
+            write_path = self.sch_ref_df.loc[school, 'tracker_path']
 
-    df = (ISR_df.merge(IS_df, how='left', on='Intervention_Session__c')
-                .drop(columns=['Intervention_Session__c'])
-                .merge(section_df, how='left', on='Section__c')
-                .drop(columns=['Section__c'])
-                .merge(school_df, how='left', on='School__c')
-                .drop(columns=['School__c'])
-                .merge(staff_df, how='left', 
-                       left_on='Intervention_Primary_Staff__c',
-                       right_on='Staff__c')
-                .drop(columns=['Intervention_Primary_Staff__c', 'Staff__c'])
-                .merge(program_df, how='left', on='Program__c')
-                .drop(columns=['Program__c']))
+            if not write_path.parent.exists() and self.test:
+                write_path.parent.mkdir(parents=True)
 
-    df['Intervention_Session_Date__c'] = \
-        pd.to_datetime(df['Intervention_Session_Date__c']).dt.date
-    df['CreatedDate'] = pd.to_datetime(df['CreatedDate']).dt.date
-    df['Comments__c'] = df['Comments__c'].fillna('', inplace=True)
+            (errors_df.loc[errors_df['School'] == school]
+                      .drop(columns='School')
+                      .to_excel(write_path, index=False))
 
-    df.loc[df['Program__c_Name'].str.contains('Tutoring')
-           & ~df['Comments__c'].str.contains('T1|T2'), 'Missing T1/T2 Code'] = 'Missing T1/T2 Code'
+        # write aggregate table
+        counts = (errors_df.groupby(['School', 'Error'])['ACM']
+                           .count()
+                           .reset_index(level='Error')
+                           .rename(columns={'ACM':'Count'}))
+        counts.to_excel(f'Z:/Impact Analytics Team/{YEAR} '
+                        'ToT Audit Error Counts.xlsx')
+        return counts
 
-    df.loc[df['Program__c_Name'].str.contains('Tutoring')
-           & df['Comments__c'].str.contains('T1')
-           & df['Comments__c'].str.contains('T2'), 'Listed T1 and T2'] = 'Listed T1 and T2'
+    def fix_T1T2ELT_typos(self):
+        df = self.get_T1T2ELT_typo_fixes_df()
 
-    df.loc[df['Program__c_Name'].str.contains('Tutoring')
-           & (df['Amount_of_Time__c'] < 10), '<10 Minutes'] = '<10 Minutes'
+        if df.empty:
+            return None
 
-    df.loc[df['Program__c_Name'].str.contains('Tutoring')
-           & (df['Amount_of_Time__c'] > 120), '>120 Minutes'] = '>120 Minutes'
+        logger.info(f"Fixing {len(df)} T1, T2, or ELT typos")
 
-    df.loc[df['Intervention_Session_Date__c'] > df['CreatedDate'],
-           'Logged in Future'] = 'Logged in Future'
+        for _, row in df.iterrows():
+            result = cysh.sf.Intervention_Session__c.update(
+                row.Intervention_Session__c,
+                {'Comments__c':row['Comments__c_fixed']}
+            )
 
-    df.loc[df['Program__c_Name'].isin(['DESSA', 'Math Inventory', 'Reading Inventory']), 'Wrong Section'] = 'Wrong Section'
+            if not isinstance(result, int) or result != 204:
+                logger.warning(f'T1, T2, ELT fix failed for '
+                               f'{row.Intervention_Session__c}: {result}')
 
-    error_cols = ['Missing T1/T2 Code', 'Listed T1 and T2', '<10 Minutes', '>120 Minutes', 'Logged in Future', 'Wrong Section']
+    @staticmethod
+    def get_T1T2ELT_typo_fixes_df():
+        """ Standardize common spellings of "T1" "T2" and "ELT"
+        """
+        typo_map = {
+            'T1': r'[Tt](?:[Ii][Ee]|[Ee][Ii])[Rr] ?(?:1|[Oo][Nn][Ee])|t1',
+            'T2': r'[Tt](?:[Ii][Ee]|[Ee][Ii])[Rr] ?(?:2|[Tt][Ww][Oo])|t2',
+        #     'ELT': r'([Aa]fter ?[Ss]chool|ASP)',
+        }
 
-    df['Error'] = df[error_cols].apply(lambda x: x.str.cat(sep=' & '), axis=1)
+        all_typos = '|'.join(list(typo_map.values()))
 
-    accepted_errors_df = pd.read_excel((
-        f"Z:/ChiPrivate/Chicago Data and Evaluation/{YEAR}/"
-        f"{YEAR} ToT Audit Accepted Errors.xlsx"
-    ))
+        df = cysh.get_object_df(
+            'Intervention_Session__c', 
+            ['Id', 'Comments__c'],
+            rename_id=True
+        )
+        df = df.loc[df['Comments__c'].str.contains(all_typos)==True]
 
-    df = df.loc[
-        (df['Error'] != '') &
-        ~df['Intervention_Session__c_Name'].isin(accepted_errors_df['SESSION_ID'])
-    ]
+        df['Comments__c_fixed'] = df['Comments__c']
+        for k, v in typo_map.items():
+            df.loc[:, 'Comments__c_fixed'] = \
+                df['Comments__c_fixed'].replace(regex=v, value=k)
 
-    col_friendly_names = {
-        'School_Name__c':'School',
-        'Staff__c_Name':'ACM',
-        'Program__c_Name':'Program',
-        'Intervention_Session__c_Name':'Session ID',
-        'Related_Student_s_Name__c':'Student',
-        'CreatedDate':'Submission Date',
-        'Intervention_Session_Date__c':'Session Date',
-        'Amount_of_Time__c':'ToT',
-        #'Comments__c':'Comment',
-        'Error':'Error',
-    }
+        return df
 
-    df = (df.rename(columns=col_friendly_names)
-            .sort_values(list(col_friendly_names.values())))
+    @staticmethod
+    def get_errors_df():
+        ISR_df = cysh.get_object_df(
+            'Intervention_Session_Result__c',
+            ['Amount_of_Time__c', 'IsDeleted', 'Intervention_Session_Date__c',
+             'Related_Student_s_Name__c', 'Intervention_Session__c', 
+             'CreatedDate']
+        )
+        IS_df = cysh.get_object_df(
+            'Intervention_Session__c',
+            ['Id', 'Name', 'Comments__c', 'Section__c'],
+            rename_id=True, rename_name=True
+        )
+        section_df = cysh.get_object_df(
+            'Section__c', 
+            ['Id', 'School__c', 'Intervention_Primary_Staff__c', 'Program__c'],
+            rename_id=True
+        )
+        school_df = cysh.get_object_df('Account', ['Id', 'Name'])
+        school_df = school_df.rename(columns={
+            'Id': 'School__c', 
+            'Name': 'School_Name__c'}
+        )
+        staff_df = cysh.get_object_df(
+            'Staff__c', 
+            ['Id', 'Name'], 
+            where="Site__c = 'Chicago'", 
+            rename_id=True, rename_name=True
+        )
+        program_df = cysh.get_object_df(
+            'Program__c', 
+            ['Id', 'Name'], 
+            rename_id=True, rename_name=True
+        )
 
-    return df[list(col_friendly_names.values())]
+        df = (ISR_df.merge(IS_df, how='left', on='Intervention_Session__c')
+                    .drop(columns=['Intervention_Session__c'])
+                    .merge(section_df, how='left', on='Section__c')
+                    .drop(columns=['Section__c'])
+                    .merge(school_df, how='left', on='School__c')
+                    .drop(columns=['School__c'])
+                    .merge(staff_df, how='left', 
+                        left_on='Intervention_Primary_Staff__c',
+                        right_on='Staff__c')
+                    .drop(columns=['Intervention_Primary_Staff__c', 
+                                   'Staff__c'])
+                    .merge(program_df, how='left', on='Program__c')
+                    .drop(columns=['Program__c']))
 
+        df['Intervention_Session_Date__c'] = \
+            pd.to_datetime(df['Intervention_Session_Date__c']).dt.date
+        df['CreatedDate'] = pd.to_datetime(df['CreatedDate']).dt.date
+        df['Comments__c'] = df['Comments__c'].fillna('')
 
-def write_error_tables_to_cyconnect(df):
-    sch_ref_df = get_sch_ref_df()
-    for _, row in sch_ref_df.iterrows():
-        school_error_df = df.loc[df['School'] == row['School']].copy()
-        del school_error_df['School']
+        error_masks = {
+            'Missing T1/T2 Code': (
+                df['Program__c_Name'].str.contains('Tutoring')
+                & ~df['Comments__c'].str.contains('T1|T2')
+            ),
+            'Listed T1 and T2': (
+                df['Program__c_Name'].str.contains('Tutoring')
+                & df['Comments__c'].str.contains('T1')
+                & df['Comments__c'].str.contains('T2')
+            ),
+            '<10 Minutes': (
+                df['Program__c_Name'].str.contains('Tutoring')
+                & (df['Amount_of_Time__c'] < 10)
+            ),
+            '>120 Minutes': (
+                df['Program__c_Name'].str.contains('Tutoring')
+                & (df['Amount_of_Time__c'] > 120)
+            ),
+            'Logged in Future': (
+                df['Intervention_Session_Date__c'] > df['CreatedDate']
+            ),
+            'Wrong Section': (
+                df['Program__c_Name'].isin(['DESSA', 'Math Inventory',
+                                            'Reading Inventory'])
+            ),
+        }
 
-        write_path = (f"Z:/{row['Informal Name']} Team Documents/"
-                      f"{YEAR} ToT Audit Errors - {row['Informal Name']}.xlsx")
+        for error_name, mask in error_masks.items():
+            df.loc[mask, error_name] = error_name
 
-        if Path(write_path).exists():
-            Path(write_path).unlink()
+        error_cols = list(error_masks.keys())
 
-        school_error_df.to_excel(write_path, index=False)
+        df['Error'] = \
+            df[error_cols].apply(lambda x: x.str.cat(sep=' & '), axis=1)
+
+        accepted_errors_df = pd.read_excel((
+            f"Z:/ChiPrivate/Chicago Data and Evaluation/{YEAR}/"
+            f"{YEAR} ToT Audit Accepted Errors.xlsx"
+        ))
+
+        df = df.loc[(df['Error'] != '') &
+                    ~df['Intervention_Session__c_Name'].isin(
+                        accepted_errors_df['SESSION_ID'])]
+
+        col_friendly_names = {
+            'School_Name__c':'School',
+            'Staff__c_Name':'ACM',
+            'Program__c_Name':'Program',
+            'Intervention_Session__c_Name':'Session ID',
+            'Related_Student_s_Name__c':'Student',
+            'CreatedDate':'Submission Date',
+            'Intervention_Session_Date__c':'Session Date',
+            'Amount_of_Time__c':'ToT',
+            #'Comments__c':'Comment',
+            'Error':'Error',
+        }
+
+        df = (df.rename(columns=col_friendly_names)
+                .sort_values(list(col_friendly_names.values())))
+
+        return df[list(col_friendly_names.values())]
